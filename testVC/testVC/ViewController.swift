2f53c41227bba341f021e05e7d54c54f05a859f1
park <park@fenrir.co.jp>
2018-03-30 18:13:58 +0900

    aaaa

---
+++ testVC/testVC/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  testVC
+//
+//  Created by 朴 基馥 on 2018/03/30.
+//  Copyright © 2018年 Fenrir Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    @IBAction func testButton(_ sender: Any) {
+        UIView.transition(with: self.view, duration: 1, options: .transitionCrossDissolve, animations: {
+            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg")!)
+        })
+        
+        UIView.transition(with: (self.navigationController?.navigationBar)!, duration: 1, options: .transitionCrossDissolve, animations: {
+            self.navigationController?.navigationBar.setBackgroundImage(UIImage(named: "bg")!, for: .default)
+        })
+    }
+    
+    @IBAction func backToHome(segue: UIStoryboardSegue) {
+        
+//        UIView.transition(with: self.view, duration: 3, options: .transitionCrossDissolve, animations: {
+//        }, completion: { _ in
+//            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg")!)
+//        })
+//
+//        UIView.transition(with: (self.navigationController?.navigationBar)!, duration: 3, options: .transitionCrossDissolve, animations: {
+//            self.navigationController?.navigationBar.setBackgroundImage(UIImage(named: "bg")!, for: .default)
+//        })
+        
+        
+    }
+}
+
